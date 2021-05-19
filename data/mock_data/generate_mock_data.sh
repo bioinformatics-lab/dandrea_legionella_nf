b3b728c1a81fd8021554ea4ea02369b2ac5743bd
Davi J. Marcon (@Mxrcon) <davijosuemarcon@gmail.com>
2021-05-19 16:19:02 -0300

    changes following the commit  `d62efe0f03faec81d7ece07973b608af988cfadd` from camila_sao_paulo

---
+++ data/mock_data/generate_mock_data.sh
@@ -0,0 +1,9 @@
+set -uex
+
+touch data/mock_data/001_R1.fastq.gz
+touch data/mock_data/001_R2.fastq.gz
+touch data/mock_data/002_R1.fastq.gz
+touch data/mock_data/002_R2.fastq.gz
+touch data/mock_data/003_R1.fastq.gz
+touch data/mock_data/003_R2.fastq.gz
+touch data/mock_data/NZ_CP029563.1_Legionella_anisa.gb
